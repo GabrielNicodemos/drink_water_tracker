b8b1a02ee04457d1a438b243476a019c1a312465
Gabriel Nicodemos <gabriel.ecn04@gmail.com>
2024-08-14 12:23:04 -0300

    UPD: tests

--- backend/drink_water_tracker/tests.py
+++ backend/drink_water_tracker/tests.py
@@ -1,18 +1,1 @@
 from django.test import TestCase
-
-from .models import UserProfile, DrinkWater
-from django.utils import timezone
-import pytz
-
-
-class DrinkWaterModelTests(TestCase):
-    def setUp(self):
-        self.user = UserProfile.objects.create(name="Gabriel", weight=90)
-
-        saopaulo_tz = pytz.timezone('America/Sao_Paulo')
-        today = timezone.now().astimezone(saopaulo_tz).date()
-        DrinkWater.objects.create(
-            user=self.user,
-            date_drink_water=today,
-            quantity_ml=1000
-        )
